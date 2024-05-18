3eadf24a9a4f3ea049a8bb3d3026229686ca93d8
Joseph Madigan <jmad1112x@gmail.com>
2024-05-17 22:34:52 -0700

    Added rudimentary Frequency visualization and waveform visualization

--- AudioProcessor.js
+++ AudioProcessor.js
@@ -2,13 +2,15 @@ class AudioProcessor {
   constructor() {
     this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     this.analyser = this.audioCtx.createAnalyser();
-    this.analyser.fftSize = 2048;
+    this.analyser.fftSize = 4096;
     this.bufferLength = this.analyser.frequencyBinCount;
     this.dataArray = new Uint8Array(this.bufferLength);
+    this.source = null;
   }
 
   async initMicrophone() {
     try {
+      await this.resumeAudioContext();
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
       this.processAudioStream(stream);
     } catch (err) {
@@ -17,21 +19,41 @@ class AudioProcessor {
   }
 
   async initFile(file) {
-    const arrayBuffer = await file.arrayBuffer();
-    const audioBuffer = await this.audioCtx.decodeAudioData(arrayBuffer);
-    this.processAudioBuffer(audioBuffer);
+    try {
+      await this.resumeAudioContext();
+      const arrayBuffer = await file.arrayBuffer();
+      const audioBuffer = await this.audioCtx.decodeAudioData(arrayBuffer);
+      this.processAudioBuffer(audioBuffer);
+    } catch (err) {
+      console.error('Error processing audio file:', err);
+    }
   }
 
   processAudioStream(stream) {
+    this.stopCurrentSource();
     this.source = this.audioCtx.createMediaStreamSource(stream);
     this.source.connect(this.analyser);
   }
 
   processAudioBuffer(audioBuffer) {
+    this.stopCurrentSource();
     this.source = this.audioCtx.createBufferSource();
     this.source.buffer = audioBuffer;
     this.source.connect(this.analyser);
     this.source.start();
+  }
+
+  stopCurrentSource() {
+    if (this.source) {
+      this.source.disconnect();
+    }
+  }
+// Checks to see if audio context is suspended -- on initial startup
+  resumeAudioContext() {
+    if (this.audioCtx.state === 'suspended') {
+      return this.audioCtx.resume();
+    }
+    return Promise.resolve();
   }
 
   getFrequencyData() {
