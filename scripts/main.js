724c269f54836ce5dc77101515b0843dda31752a
Joseph Madigan <jmad1112x@gmail.com>
2024-06-16 02:28:09 -0700

    Added photo sensitivity warning modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
-import { AudioProcessor } from "../audio/AudioProcessor.js";
+import { AudioProcessor } from '../audio/AudioProcessor.js';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const audioProcessor = AudioProcessor.getInstance();
+document.addEventListener('DOMContentLoaded', () => {
+	const audioProcessor = AudioProcessor.getInstance();
 
-  const micOption = document.getElementById("mic-option");
-  const fileOption = document.getElementById("file-option");
-  const startMicButton = document.getElementById("start-mic");
-  const selectFileButton = document.getElementById("select-file");
+	const micOption = document.getElementById('mic-option');
+	const fileOption = document.getElementById('file-option');
+	const startMicButton = document.getElementById('start-mic');
+	const selectFileButton = document.getElementById('select-file');
 
-  const fileInput = document.getElementById("file-input");
-  const aframeIframe = document.getElementById("aframe-iframe");
+	const fileInput = document.getElementById('file-input');
+	const aframeIframe = document.getElementById('aframe-iframe');
 
 	function playPause() {
 		if (count % 2 === 0) {
@@ -25,6 +25,15 @@ document.addEventListener("DOMContentLoaded", () => {
 	// 	} else audioProcessor.pause();
 	// });
 
+	// Photo Sensitivity Warning
+	const modal = document.getElementById('warning-modal');
+	const acceptButton = document.getElementById('accept-button');
+
+	acceptButton.addEventListener('click', function () {
+		modal.style.display = 'none';
+		aframeIframe.style.display = 'block';
+	});
+
 	micOption.addEventListener('change', () => {
 		if (micOption.checked) {
 			startMicButton.disabled = false;
@@ -34,17 +43,17 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	});
 
-  fileOption.addEventListener("change", () => {
-    if (fileOption.checked) {
-      selectFileButton.disabled = false;
-      startMicButton.disabled = true;
-      audioProcessor.stop();
-    }
-  });
+	fileOption.addEventListener('change', () => {
+		if (fileOption.checked) {
+			selectFileButton.disabled = false;
+			startMicButton.disabled = true;
+			audioProcessor.stop();
+		}
+	});
 
-  startMicButton.addEventListener("click", () => {
-    audioProcessor.initMicrophone();
-  });
+	startMicButton.addEventListener('click', () => {
+		audioProcessor.initMicrophone();
+	});
 
 	selectFileButton.addEventListener('click', () => {
 		fileInput.click();
