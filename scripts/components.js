09efc0265b0e757dd9948fb32b681269ab0a617c
Emma Melkumian <emmamelkumian@yahoo.com>
2024-06-16 23:22:31 -0700

    new particle stage, and vr controls

--- scripts/components.js
+++ scripts/components.js
@@ -360,3 +360,25 @@ AFRAME.registerComponent("custom-camera", {
     this.el.appendChild(camera);
   },
 });
+
+// Register a thumbstick logging component
+AFRAME.registerComponent("thumbstick-logging", {
+  init: function () {
+    this.el.addEventListener("thumbstickmoved", this.logThumbstick);
+  },
+
+  logThumbstick: function (evt) {
+    if (evt.detail.y > 0.95) {
+      console.log("DOWN");
+    }
+    if (evt.detail.y < -0.95) {
+      console.log("UP");
+    }
+    if (evt.detail.x < -0.95) {
+      console.log("LEFT");
+    }
+    if (evt.detail.x > 0.95) {
+      console.log("RIGHT");
+    }
+  },
+});
